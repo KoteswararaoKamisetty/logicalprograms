7c820abe6bdb83700eed13d7ac3a1ccff0cce5e3
KoteswararaoKamisetty <145526237+KoteswararaoKamisetty@users.noreply.github.com>
2023-10-09 16:38:17 +0530

    Update rearrangeMaxMin.js

--- rearrangeMaxMin.js
+++ rearrangeMaxMin.js
@@ -1,1 +1,34 @@
+function rearrangeMaxMin(arr) {
+    // arr.sort((a, b) => a - b); // Step 1: Sort in ascending order
+    
+    for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr.length -i-1; j++){
+        if(arr[j] > arr[j+1]){
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+}
+
+    let low = 0;
+    let high = arr.length - 1;
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 === 0) {
+            result.push(arr[high]);
+            high--;
+        } else {
+            result.push(arr[low]);
+            low++;
+        }
+    }
+
+    return result;
+}
+
+let arr = [9, 3, 5, 7, 2, 4, 6, 8];
+let rearrangedArray = rearrangeMaxMin(arr);
+console.log(rearrangedArray);
 
